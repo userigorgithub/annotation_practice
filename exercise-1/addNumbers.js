310bcec31504b212e5351009025c12b68cee6aea
Eric Weissman <eric.weissman21@gmail.com>
2021-01-13 09:26:45 -0700

    Add exercise set 1

---
+++ exercise-1/addNumbers.js
@@ -0,0 +1,20 @@
+function addNumbers() {
+  var firstNum = 9
+  var secondNum = 14
+
+  firstNum = 3
+  secondNum = 4
+
+  var nums = [firstNum, secondNum]
+
+  var product = firstNum * secondNum
+  var sum = firstNum + secondNum
+  var average = sum / nums.length
+
+  console.log(nums[0])
+  console.log(nums[1])
+
+  console.log(`The first number is ${num1} and the second number is ${num2}!`)
+
+  return sum
+}
---
+++ exercise-1/add_numbers.rb
@@ -0,0 +1,20 @@
+def add_numbers() 
+  first_num = 9
+  second_num = 14
+
+  first_num = 3
+  second_num = 4
+
+  nums = [first_num, second_num]
+
+  product = first_num * second_num
+  sum = first_num + second_num
+  average = sum / nums.length
+
+  puts nums[0]
+  puts nums[1]
+
+  puts "The first number is #{first_num} and the second number is #{second_num}!"
+
+  return sum
+end

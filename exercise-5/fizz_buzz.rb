2212c5e55b10fa7303039c1d4d2be1ff8b1c2b2c
Eric Weissman <eric.weissman21@gmail.com>
2021-01-18 12:55:54 -0700

    Add fizzbuzz

---
+++ exercise-5/fizz_buzz.rb
@@ -0,0 +1,17 @@
+def fizzbuzz(num_1, num_2, range)
+  (1..range).each do |i|
+    if i % num_1 === 0 && i % num_2 === 0
+      puts 'fizzbuzz'
+    elsif i % num_1 === 0
+      puts 'fizz'
+    elsif i % num_2 === 0
+      puts 'buzz'
+    else
+      puts i
+    end
+  end
+end
+
+fizzbuzz(3, 5, 100)
+
+fizzbuzz(5, 8, 400)
